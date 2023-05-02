c7c175dde00094951368708d538e44c80959953d
1orz <cloudorzi@gmail.com>
2023-05-02 22:43:07 +0800

    fix

--- immortalwrt/prenv.sh
+++ immortalwrt/prenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo rm -rf /etc/apt/sources.list.d/*
 sudo apt update
-sudo apt install -y build-essential libncurses5-dev gawk
+sudo apt install -y build-essential libncurses5-dev gawk gettext
 sudo apt autoremove --purge
 sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 sudo df -h
